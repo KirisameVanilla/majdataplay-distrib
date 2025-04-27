8d2dd4e2cfa9ad9b2061315d008ffbc3e1913ad7
Haku <dev@mcoca.dev>
2025-04-27 15:28:10 -0400

    impl cache for files

--- src/index.js
+++ src/index.js
@@ -4,22 +4,35 @@ export default {
     const url = new URL( request.url );
     let pathname = url.pathname;
 
-    if ( pathname === "/" )
-    {
+    if ( pathname === "/" ) {
+      const headers = new Headers();
+      headers.set("Content-Type", "text/plain");
+      headers.set("Cache-Control", "public, max-age=60");
+      
       return new Response( "Welcome! Append a file name to download.", {
         status: 200,
-        headers: { "content-type": "text/plain" },
-      } );
+        headers,
+      });
     }
 
     pathname = pathname.slice( 1 ); // 去掉最前面的 /
+    const forceRefresh = url.searchParams.get('refresh') === '1';
+
+    const cache = caches.default;
+    const cacheKey = new Request(url.origin + url.pathname, request);
+
+    if (!forceRefresh) {
+      const cachedResponse = await cache.match(cacheKey);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+    }
 
     try
     {
       // 从 R2 bucket 读
       const object = await env.MAJDATA_BUCKET.get( pathname );
-      if ( !object )
-      {
+      if ( !object ) {
         return new Response( "404 Not Found", { status: 404 } );
       }
 
@@ -27,10 +40,24 @@ export default {
       headers.set( "Content-Type", object.httpMetadata?.contentType || "application/octet-stream" );
       headers.set( "Content-Disposition", "attachment" );
 
-      return new Response( object.body, { headers } );
+      if (pathname === "Nightly.json" || pathname === "Stable.json") {
+        headers.set("Cache-Control", "no-store");
+      } else if (pathname.startsWith("Stable/")) {
+        headers.set("Cache-Control", `public, max-age=${25 * 24 * 60 * 60}`); // stable: 25 days
+      } else if (pathname.startsWith("Nightly/")) {
+        headers.set("Cache-Control", `public, max-age=${3 * 60 * 60}`); // nightly: 3 hours
+      } else {
+        headers.set("Cache-Control", `public, max-age=${1 * 60 * 60}`); // version: 1 hour
+      }
 
-    } catch ( e )
-    {
+      const response = new Response(object.body, { headers });
+      if (!forceRefresh) {
+        ctx.waitUntil(cache.put(cacheKey, response.clone()));
+      }
+      
+      return response;
+
+    } catch ( e ) {
       return new Response( "Internal Error", { status: 500 } );
     }
   },
