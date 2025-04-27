83a9bc246c59f16d98faf294dc6659601365c5fd
KirisameVanilla <vanillaaaajp@gmail.com>
2025-04-28 02:42:37 +0800

    move all to bucket

--- src/index.js
+++ src/index.js
@@ -14,68 +14,24 @@ export default {
 
     pathname = pathname.slice( 1 ); // 去掉最前面的 /
 
-    // 这里：决定哪些路径用 R2
-    const serveFromAsset = pathname === "Nightly.json" || pathname === "Stable.json";
-
     try
     {
-      if ( !serveFromAsset )
+      // 从 R2 bucket 读
+      const object = await env.MAJDATA_BUCKET.get( pathname );
+      if ( !object )
       {
-        // 从 R2 bucket 读
-        const object = await env.MAJDATA_BUCKET.get( pathname ); // 这里 MY_BUCKET 是 wrangler.toml 绑定的名字
-        if ( !object )
-        {
-          return new Response( "404 Not Found", { status: 404 } );
-        }
-
-        const headers = new Headers();
-        headers.set( "Content-Type", object.httpMetadata?.contentType || "application/octet-stream" );
-        headers.set( "Content-Disposition", "attachment" );
-
-        return new Response( object.body, { headers } );
-      } else
-      {
-        // 从原本的 ASSETS 读
-        const manifest = JSON.parse( env.__STATIC_CONTENT_MANIFEST );
-        const assetKey = manifest[pathname];
-
-        if ( !assetKey )
-        {
-          return new Response( "404 Not Found", { status: 404 } );
-        }
-
-        const assetBody = await env.__STATIC_CONTENT.get( assetKey, { type: 'arrayBuffer' } );
-        if ( !assetBody )
-        {
-          return new Response( "404 Not Found", { status: 404 } );
-        }
-
-        const headers = new Headers();
-        headers.set( "Content-Type", getContentType( pathname ) );
-        headers.set( "Content-Disposition", "attachment" );
-
-        return new Response( assetBody, { headers } );
+        return new Response( "404 Not Found", { status: 404 } );
       }
+
+      const headers = new Headers();
+      headers.set( "Content-Type", object.httpMetadata?.contentType || "application/octet-stream" );
+      headers.set( "Content-Disposition", "attachment" );
+
+      return new Response( object.body, { headers } );
+
     } catch ( e )
     {
       return new Response( "Internal Error", { status: 500 } );
-    }
-    function getContentType ( pathname )
-    {
-      if ( pathname.endsWith( ".json" ) )
-      {
-        return "application/json";
-      }
-      if ( pathname.endsWith( ".txt" ) )
-      {
-        return "text/plain";
-      }
-      if ( pathname.endsWith( ".html" ) )
-      {
-        return "text/html";
-      }
-      // 其他情况
-      return "application/octet-stream";
     }
   },
 };
